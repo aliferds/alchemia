5cce21bcc6f0dca3487f48bcb55840436ccee837
aliferds <aliferduartesilva@gmail.com>
2025-10-27 17:37:24 -0300

    Change: svg code

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,3 +1,7 @@
+import { Air } from "@/components/elements/air"
+import { Earth } from "@/components/elements/earth"
+import { Fire } from "@/components/elements/fire"
+import { Water } from "@/components/elements/water"
 import { HomeLink } from "@/components/homeLink"
 import { Ouroboros } from "@/components/ouroboros"
 
@@ -10,9 +14,15 @@ const GamePage = () => {
         <HomeLink />
       </div>
       <div className="fixed flex items-center justify-evenly bottom-4 left-1/2 translate-x-[-50%] w-full max-w-[320px] h-[56px] rounded-xl border-2 border-foreground">
-        <div className="w-[48px] h-[48px] border-white border-2 rounded-full cursor-pointer"></div>
-        <div className="w-[48px] h-[48px] border-white border-2 rounded-full cursor-pointer"></div>
-        <div className="w-[48px] h-[48px] border-white border-2 rounded-full cursor-pointer"></div>
+        <div className="flex justify-center items-center w-[48px] h-[48px] border-white border-2 rounded-full cursor-pointer">
+          <Fire />
+        </div>
+        <div className="flex justify-center items-center w-[48px] h-[48px] border-white border-2 rounded-full cursor-pointer">
+          <Earth />
+        </div>
+        <div className="flex justify-center items-center w-[48px] h-[48px] border-white border-2 rounded-full cursor-pointer">
+          <Air />
+        </div>
         <button className="flex justify-center items-center w-[48px] h-[48px] border-white border-2 bg-violet-600 cursor-pointer rounded-full">
           <Ouroboros size="32px"/>
         </button>
